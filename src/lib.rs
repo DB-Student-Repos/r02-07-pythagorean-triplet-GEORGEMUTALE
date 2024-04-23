8a647c20997d5d20d2a792accc7a9017c3176b68
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-04-23 20:55:54 +0200

    more simplified

--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,14 @@
 use std::collections::HashSet;
-
-pub fn find(sum: u32) -> HashSet<[u32; 3]> {
-    // Create a HashSet to store the triplets
+pub fn find(m: u32) -> HashSet<[u32; 3]> {
     let mut triplets = HashSet::new();
-    
-    // Iterate over possible values of 'a'
-    for a in 1..=sum / 3 {
-        // Calculate the denominator and numerator based on the parametrization
-        let denom = 2 * (sum - a);
-        let num = sum * sum - 2 * a * sum;
-        
-        // Check if the numerator is divisible by the denominator
-        if num % denom == 0 {
-            // Calculate 'b' from the division
-            let b = num / denom;
-            
-            // Check if 'a' is less than 'b'
-            if a < b {
-                // Insert the triplet into the HashSet
-                triplets.insert([a, b, sum - a - b]);
+    let sum = m as i32;
+    for a in 1i32..sum {
+        for b in a..sum {
+            let c = sum - b - a;
+            if c > b && (a.pow(2) + b.pow(2) == c.pow(2)) {
+                triplets.insert([a as u32, b as u32, c as u32]);
             }
         }
     }
-
-    // Return the HashSet containing all found Pythagorean triplets
     triplets
 }
