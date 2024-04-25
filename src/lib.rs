028c8b10b574f34b62e26c1b16d3dea2d7bae01f
GEORGEMUTALE <mutalegeorge367@gmail.com>
2024-04-25 23:15:37 +0200

    Modified solution

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 use std::collections::HashSet;
-pub fn find(m: u32) -> HashSet<[u32; 3]> {
+
+pub fn find(n: u32) -> HashSet<[u32; 3]> {
     let mut triplets = HashSet::new();
-    let sum = m as i32;
-    for a in 1i32..sum {
-        for b in a..sum {
-            let c = sum - b - a;
-            if c > b && (a.pow(2) + b.pow(2) == c.pow(2)) {
-                triplets.insert([a as u32, b as u32, c as u32]);
+    for a in 1..(n / 3) {
+        for b in (a + 1)..(n / 2) {
+            let c = n - a - b;
+            if a * a + b * b == c * c {
+                triplets.insert([a, b, c]);
             }
         }
     }
